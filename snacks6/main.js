f59c4008c1050d0f701669386002f1145824b47d
Stefano <stefano2890@gmail.com>
2020-04-23 17:40:49 +0200

    snacks 6 done

---
+++ snacks6/main.js
@@ -0,0 +1,5 @@
+// stampa le potenze di 2 fino a che il risultato di 2 elevato a N sarà uguale o minore di 1000;
+// creo un ciclo che mi moltiplica ad ogni passaggio il numero precendetemente moltiplicato per 2 e lo moltiplica a sua volta per 2 fino a che il risultato non supererà la cifra 1000;
+for (var i = 1; i <= 1000; i = i * 2) {
+    console.log(i);
+}
